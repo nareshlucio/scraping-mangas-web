1bf414910be29310383af8be9650846d1641284a
nareshlucio-aspentech <david.lucio@aspentech.com>
2023-12-05 13:25:07 -0600

    re-estructurando proyecto

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import '../styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,10 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel='noopener'>
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel='noopener'>
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
